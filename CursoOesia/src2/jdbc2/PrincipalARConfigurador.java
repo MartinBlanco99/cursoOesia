0c237dea5d3bd16e23739408ec4cf2b37a2323bf
java <java@10.0.2.15>
2022-08-29 14:34:59 +0200

    PersonaAR- Configurador

---
+++ CursoOesia/src2/jdbc2/PrincipalARConfigurador.java
@@ -0,0 +1,12 @@
+package jdbc2;
+
+public class PrincipalARConfigurador {
+
+	public static void main(String[] args) {
+		
+		
+		PersonaAR fernando= new PersonaAR("100");
+		fernando.borrar();
+
+}
+}
