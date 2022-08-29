a5dee37ab2f22ebf10549abce9175bdfab2e429a
java <java@10.0.2.15>
2022-08-29 11:10:20 +0200

    Inserción de datos

--- CursoOesia/src2/jdbc/Principal3.java
+++ CursoOesia/src2/jdbc/Principal3.java
@@ -2,7 +2,6 @@ package jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -15,25 +14,29 @@ public class Principal3 {
 	static final String CONSULTA = "SELECT * FROM Personas";
 
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
 
-		try (Connection conn = DriverManager.getConnection(URL, USUARIO, CLAVE);
-				Statement sentencia = conn.createStatement();
-				ResultSet rs = sentencia.executeQuery(CONSULTA);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("dame el dni");
+			String dni = sc.next();
 
-		) {
+			System.out.println("dame el nombre");
+			String nombre = sc.next();
 
-			while (rs.next()) {
-				System.out.println(rs.getString("dni"));
-				System.out.println(rs.getString("nombre"));
-				System.out.println(rs.getString("apellidos"));
+			System.out.println("dame apellidos");
+			String apellidos = sc.next();
+			String consulta2 = "insert into personas values('" + dni + "','" + nombre + "','" + apellidos + "')";
+			try (Connection conn = DriverManager.getConnection(URL, USUARIO, CLAVE);
+					Statement sentencia = conn.createStatement();
 
+			) {
+
+				sentencia.execute(consulta2);
+				System.out.println("Datos insertados");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-
-			System.out.println("estamos conectados");
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 
 	}
