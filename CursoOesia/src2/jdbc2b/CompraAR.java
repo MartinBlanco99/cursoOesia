7c264691659478ad40e6a1f348ac8579c435b2bb
java <java@10.0.2.15>
2022-08-30 10:40:43 +0200

    Actualizacion CompraAR

--- CursoOesia/src2/jdbc2b/CompraAR.java
+++ CursoOesia/src2/jdbc2b/CompraAR.java
@@ -14,6 +14,10 @@ public class CompraAR {
 	static final String SELECCIONAR = "SELECT * FROM Compras";
 	static final String INSERCION = "Insert into Compras (concepto,importe,personas_dni) values(?,?,?)";
 	static final String BORRAR = "DELETE from Compras where id=?";
+	static final String ORDENARIMPORTE = "SELECT * FROM Compras ORDER BY importe ";
+	static final String ORDENARCONCEPTO = "SELECT * FROM Compras ORDER BY concepto ";
+	static final String ORDENAR = "SELECT * FROM Compras ORDER BY %s ";
+
 
 	private int id;
 	private String concepto;
@@ -98,7 +102,7 @@ public class CompraAR {
 
 		List<CompraAR> listaCompras = new ArrayList<CompraAR>();
 
-		try (PreparedStatement sentencia = DataBaseHelper.crearSentenciaPreparada(SELECCIONAR, null);
+		try (PreparedStatement sentencia = DataBaseHelper.crearSentenciaPreparada(ORDENARIMPORTE, null);
 				Connection conn = sentencia.getConnection();) {
 
 			try (ResultSet rs = sentencia.executeQuery();) {
@@ -143,6 +147,96 @@ public class CompraAR {
 		} catch (SQLException e) {
 			throw new RuntimeException("error de datos", e);
 		}
+
+	}
+	
+	public static List<CompraAR> buscarTodosPorImporte() {
+
+		List<CompraAR> listaCompras = new ArrayList<CompraAR>();
+
+		try (PreparedStatement sentencia = DataBaseHelper.crearSentenciaPreparada(SELECCIONAR, null);
+				Connection conn = sentencia.getConnection();) {
+
+			try (ResultSet rs = sentencia.executeQuery();) {
+
+				while (rs.next()) {
+					CompraAR compraAR = new CompraAR();
+					compraAR.setId(rs.getInt("id"));
+					compraAR.setConcepto(rs.getString("concepto"));
+					compraAR.setImporte(rs.getDouble("importe"));
+					compraAR.setDni(rs.getString("personas_dni"));
+					
+					listaCompras.add(compraAR);
+				}
+			} catch (SQLException e) {
+				throw new RuntimeException("error de datos", e);
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException("error de datos", e);
+		}
+
+		return listaCompras;
+
+	}
+	
+	public static List<CompraAR> buscarTodosPorConcepto() {
+
+		List<CompraAR> listaComprasConcepto = new ArrayList<CompraAR>();
+
+		try (PreparedStatement sentencia = DataBaseHelper.crearSentenciaPreparada(ORDENARCONCEPTO, null);
+				Connection conn = sentencia.getConnection();) {
+
+			try (ResultSet rs = sentencia.executeQuery();) {
+
+				while (rs.next()) {
+					CompraAR compraAR = new CompraAR();
+					compraAR.setId(rs.getInt("id"));
+					compraAR.setConcepto(rs.getString("concepto"));
+					compraAR.setImporte(rs.getDouble("importe"));
+					compraAR.setDni(rs.getString("personas_dni"));
+					
+					listaComprasConcepto.add(compraAR);
+				}
+			} catch (SQLException e) {
+				throw new RuntimeException("error de datos", e);
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException("error de datos", e);
+		}
+
+		return listaComprasConcepto;
+
+	}
+	
+	public static List<CompraAR> buscarTodosOrdenados(String campo) {
+
+		List<CompraAR> listaPersonas = new ArrayList<CompraAR>();
+
+		String SQL_Final= String.format(ORDENAR,campo);
+		try (PreparedStatement sentencia = DataBaseHelper.crearSentenciaPreparada(SQL_Final);
+				Connection conn = sentencia.getConnection();) {
+
+			try (ResultSet rs = sentencia.executeQuery();) {
+
+				while (rs.next()) {
+					CompraAR compra = new CompraAR();
+					compra.setId(rs.getInt("id"));
+					compra.setConcepto(rs.getString("concepto"));
+					compra.setImporte(rs.getDouble("importe"));
+					compra.setDni(rs.getString("personas_dni"));
+					listaPersonas.add(compra);
+				}
+			} catch (SQLException e) {
+				throw new RuntimeException("error de datos", e);
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException("error de datos", e);
+		}
+
+		return listaPersonas;
 
 	}
 
--- CursoOesia/src2/jdbc2b/PrincipalCompraAR.java
+++ CursoOesia/src2/jdbc2b/PrincipalCompraAR.java
@@ -5,27 +5,83 @@ import java.util.List;
 public class PrincipalCompraAR {
 
 	public static void main(String[] args) {
-		
+
 		List<CompraAR> lista = CompraAR.buscarTodos();
-		
-		for(CompraAR c : lista ) {
+
+		for (CompraAR c : lista) {
 			System.out.println(c.getId());
 			System.out.println(c.getConcepto());
 			System.out.println(c.getImporte());
 			System.out.println(c.getDni());
 
-			
 		}
-		
-		//CompraAR c1 = new CompraAR("Reloj",200,"4");
-		//c1.insertar();
-		
-		CompraAR c2 = new CompraAR("Borrar",1000,"60");
-		//c2.insertar();
-		//c2.borrar();
-		
+
+		System.out.println();
+
+		// CompraAR c1 = new CompraAR("Reloj",200,"4");
+		// c1.insertar();
+
+		CompraAR c2 = new CompraAR("Borrar", 1000, "60");
+		// c2.insertar();
+		// c2.borrar();
+
 		CompraAR cBorrar = new CompraAR(1);
 		cBorrar.borrar();
 
-}
+		System.out.println();
+
+		List<CompraAR> listaImportes = CompraAR.buscarTodosPorImporte();
+
+		for (CompraAR c : listaImportes) {
+			System.out.println(c.getId());
+			System.out.println(c.getConcepto());
+			System.out.println(c.getImporte());
+			System.out.println(c.getDni());
+
+		}
+
+		System.out.println();
+
+		List<CompraAR> listaConceptos = CompraAR.buscarTodosPorConcepto();
+
+		for (CompraAR c : listaConceptos) {
+			System.out.println(c.getId());
+			System.out.println(c.getConcepto());
+			System.out.println(c.getImporte());
+			System.out.println(c.getDni());
+
+		}
+
+		System.out.println();
+		List<CompraAR> lista2 = CompraAR.buscarTodosOrdenados("importe");
+
+		for (CompraAR c : lista2) {
+
+			System.out.println(c.getDni());
+			System.out.println(c.getConcepto());
+			System.out.println(c.getImporte());
+		}
+		
+		System.out.println();
+		List<CompraAR> lista3 = CompraAR.buscarTodosOrdenados("personas_dni");
+
+		for (CompraAR c : lista3) {
+
+			System.out.println(c.getDni());
+			System.out.println(c.getConcepto());
+			System.out.println(c.getImporte());
+		}
+		
+		System.out.println();
+		List<CompraAR> lista4 = CompraAR.buscarTodosOrdenados("concepto");
+
+		for (CompraAR c : lista4) {
+
+			System.out.println(c.getDni());
+			System.out.println(c.getConcepto());
+			System.out.println(c.getImporte());
+		}
+
+
+	}
 }
