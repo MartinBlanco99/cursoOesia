277a8407d6e2f407295b726d5074ed45a7802526
java <java@10.0.2.15>
2022-09-08 14:34:54 +0200

    GestorGasto actualizaciones

--- CursoMavenOesia/src/test/java/arquitecturajava/utils/GestorGastoTest.java
+++ CursoMavenOesia/src/test/java/arquitecturajava/utils/GestorGastoTest.java
@@ -13,34 +13,47 @@ class GestorGastoTest {
 	void getImporte2CuotasTest() {
 		Gasto g1 = mock(Gasto.class);
 		Gasto g2 = mock(Gasto.class);
-		
 
 		GestorGasto gestorGasto = new GestorGasto(g1, g2);
-		
+
 		when(g1.getImporte2Cuotas()).thenReturn(50.0);
 		when(g2.getImporte2Cuotas()).thenReturn(50.0);
 
 		assertEquals(105, gestorGasto.getImporte2Cuotas());
-		
+
 		verify(g1).getImporte2Cuotas();
 		verify(g2).getImporte2Cuotas();
 	}
-	
+
 	@Test
 	void getImporte4CuotasTest() {
 		Gasto g1 = mock(Gasto.class);
 		Gasto g2 = mock(Gasto.class);
-		
 
 		GestorGasto gestorGasto = new GestorGasto(g1, g2);
-		
+
 		when(g1.getImporte4Cuotas()).thenReturn(50.0);
 		when(g2.getImporte4Cuotas()).thenReturn(50.0);
 
 		assertEquals(105, gestorGasto.getImporte4Cuotas());
-		
+
 		verify(g1).getImporte4Cuotas();
 		verify(g2).getImporte4Cuotas();
 	}
 
+	@Test
+	void multipleCuotasTest() {
+		Gasto g1 = mock(Gasto.class);
+		Gasto g2 = mock(Gasto.class);
+
+		GestorGasto gestorGasto = new GestorGasto(g1, g2);
+
+		when(g1.getImporteCuotas(3)).thenReturn(50.0);
+		when(g2.getImporteCuotas(3)).thenReturn(50.0);
+
+		assertEquals(105, gestorGasto.getImporteCuotas(3));
+
+		// verify(g1).getImporteCuotas();
+		// verify(g2).getImporteCuotas();
+	}
 }
