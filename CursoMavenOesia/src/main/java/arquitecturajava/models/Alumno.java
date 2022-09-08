6f8f50d17683c457b08fff06e3096f5807949bbb
java <java@10.0.2.15>
2022-09-08 08:50:13 +0200

    Nuevos metodos para AlumnoTest2

--- CursoMavenOesia/src/main/java/arquitecturajava/models/Alumno.java
+++ CursoMavenOesia/src/main/java/arquitecturajava/models/Alumno.java
@@ -16,7 +16,6 @@ public class Alumno {
 		// TODO Auto-generated constructor stub
 	}
 
-
 	public String getNombre() {
 		return nombre;
 	}
@@ -24,8 +23,6 @@ public class Alumno {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
-	
 
 	public List<Nota2> getNotasLista() {
 		return notasLista;
@@ -38,7 +35,7 @@ public class Alumno {
 	public void addNota(Nota2 nota) {
 		notasLista.add(nota);
 	}
-	
+
 	public void removeNota(Nota2 nota) {
 		notasLista.remove(nota);
 	}
@@ -51,12 +48,29 @@ public class Alumno {
 		}
 		return total / notasLista.size() >= 5;
 	}
-	
-//	public void getNotaMayor() {
-//		int notaMayor;
-//		if(notasLista.)
-//		
-//		notasLista.get(0);
-//	}
+
+	public Nota2 notaMayor() {
+
+		Nota2 notaMayor = notasLista.get(0);
+
+		for (Nota2 n : notasLista) {
+			if (notaMayor.getValor() < n.getValor()) {
+				notaMayor = n;
+			}
+		}
+		return notaMayor;
+
+	}
+
+	public List<Nota2> suspensos() {
+		List<Nota2> suspensos = new ArrayList<Nota2>();
+
+		for (Nota2 n : notasLista) {
+			if (n.getValor() < 5) {
+				suspensos.add(n);
+			}
+		}
+		return suspensos;
+	}
 
 }
--- CursoMavenOesia/src/test/java/arquitecturajava/models/AlumnoTest2.java
+++ CursoMavenOesia/src/test/java/arquitecturajava/models/AlumnoTest2.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 class AlumnoTest2 {
 
 	Alumno a;
+	List<Nota2>notas;
 
 	@BeforeEach
 	public void setUp() {
@@ -21,50 +22,59 @@ class AlumnoTest2 {
 		a.setNotasLista(listaNotasIncial());
 	}
 
-	private List<Nota2> listaNotasIncial() {
-		List<Nota2> notas = new ArrayList<Nota2>();
-		Nota2 n = new Nota2(6, "lengua");
-		Nota2 n2 = new Nota2(4, "ingles");
-		notas.add(n);
-		notas.add(n2);
-
-		Collections.addAll(notas, n, n2);
-		return notas;
-	}
-
 	@Test
 	public void addNotaTest() {
-		// a.addNota(new Nota2(6, "frances"));
-		Nota2 n0 = new Nota2(5, "galego");
-		a.addNota(n0);
-		// assertEquals(3, a.getNotasLista().size());
-		assertTrue(a.getNotasLista().contains(n0));
+		Nota2 nota = new Nota2(5, "galego");
+		assertFalse(a.getNotasLista().contains(nota));
+		a.addNota(nota);
+		assertTrue(a.getNotasLista().contains(nota));
 	}
 
 	@Test
 	public void removeNotaTest() {
-		Nota2 nota = new Nota2(4, "ingles");
+		Nota2 nota = new Nota2(5, "galego");
 		a.removeNota(nota);
-		assertFalse(!a.getNotasLista().contains(nota));
+		assertFalse(a.getNotasLista().contains(nota));
 	}
 
 	@Test
 	public void cursoAptoTest() {
-		List<Nota2> notas = listaNotasMedia();
-
+		List<Nota2> notas = listaNotasParaMedia();
 		a.setNotasLista(notas);
 		assertTrue(a.apruebaElCurso());
 	}
 
-	private List<Nota2> listaNotasMedia() {
-		Nota2 n1 = new Nota2(6, "lengua");
-		Nota2 n2 = new Nota2(4, "ingles");
-		Nota2 n3 = new Nota2(3, "lengua");
-		Nota2 n4 = new Nota2(8, "ingles");
+	@Test
+	void alumnoNotaMayorTest() {
+		List<Nota2> notas = listaNotasParaMedia();
+		a.setNotasLista(notas);
+		assertEquals(new Nota2(8, "lengua"), a.notaMayor());
 
-		List<Nota2> notas = new ArrayList<Nota2>();
-		Collections.addAll(notas, n1, n2, n3, n4);
+	}
+
+	@Test
+	void notasSuspensasTest() {
+		List<Nota2> notas = listaNotasParaMedia();
+		a.setNotasLista(notas);
+		List<Nota2> notasSuspensas = a.suspensos();
+		notasSuspensas.forEach((nota) -> assertTrue(nota.getValor() < 5));
+	}
+
+	private List<Nota2> listaNotasIncial() {
+		notas = new ArrayList<Nota2>();
+		Nota2 n = new Nota2(6, "lengua");
+		Nota2 n2 = new Nota2(4, "ingles");
+		Nota2 n3 = new Nota2(4, "lengua");
+		Collections.addAll(notas, n, n2,n3);
 		return notas;
 	}
 
+	private List<Nota2> listaNotasParaMedia() {
+		Nota2 n1 = new Nota2(6, "lengua");
+		Nota2 n2 = new Nota2(4, "ingles");
+		Nota2 n3 = new Nota2(8, "lengua");
+		List<Nota2> notas = new ArrayList<Nota2>();
+		Collections.addAll(notas, n1, n2, n3);
+		return notas;
+	}
 }
