bc971d85f8037f770ba063f5f78228057d16e775
java <java@10.0.2.15>
2022-09-14 13:00:58 +0200

    Ejemplo de debug

---
+++ CursoMavenOesia/src/main/java/arquitecturajava/proyecto3/Principal.java
@@ -0,0 +1,23 @@
+package arquitecturajava.proyecto3;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		int numero = 10;
+		
+		
+		
+		Persona p = new Persona("martin", 23);
+		System.out.println(p);
+		
+		
+		
+		System.out.println("Programa 0");
+		System.out.println("Programa 1");
+		System.out.println(numero);
+		System.out.println("Programa 3");
+
+	}
+
+}
